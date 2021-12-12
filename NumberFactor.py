02ba510d1d3fd5f0e4750fd293399c4787950959
shatheesh17 <36799558+shatheesh17@users.noreply.github.com>
2021-12-12 11:27:21 +0530

    number factor

---
+++ NumberFactor.py
@@ -0,0 +1,12 @@
+def numberFactor(n):
+    if n in (0,1,2):
+        return 1
+    elif n==3:
+        return 2
+    else:
+        subP1=numberFactor(n-1)
+        subP2=numberFactor(n-3)
+        subP3=numberFactor(n-4)
+        return subP3+subP2+subP1
+
+print(numberFactor(5))
